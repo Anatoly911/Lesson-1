367c63b4bb4ca7d15a6575fc40a6b0e8eef83fec
Anatolii Revutskyi <revutskyanatoly@gmail.com>
2022-12-26 22:44:20 +0200

    Lesson 8

--- Restaurant.Notification/Consumers/KitchenReadyConsumer.cs
+++ Restaurant.Notification/Consumers/KitchenReadyConsumer.cs
@@ -1,4 +1,5 @@
 ﻿using MassTransit;
+using Microsoft.Extensions.Logging;
 using Restaurant.Messages;
 using System;
 using System.Collections.Generic;
@@ -10,13 +11,16 @@ namespace Restaurant.Notification.Consumers
 {
     public class KitchenReadyConsumer : IConsumer<IKitchenReady>
     {
+        private ILogger _logger;
         private readonly Notifier _notifier;
-        public KitchenReadyConsumer(Notifier notifier)
+        public KitchenReadyConsumer(ILogger<KitchenReadyConsumer> logger, Notifier notifier)
         {
             _notifier = notifier;
+            _logger = logger;
         }
         public Task Consume(ConsumeContext<IKitchenReady> context)
         {
+            _logger.Log(LogLevel.Information, $"[OrderId: {context.Message.OrderId}]");
             _notifier.Accept(context.Message.OrderId, Accepted.Kitchen);
             return Task.CompletedTask;
         }
